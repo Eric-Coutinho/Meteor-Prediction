d475c1863579e3108acf3cc1ed3edc8027268c55
Eric Coutinho <eric.coutinhop@hotmail.com>
2024-03-28 14:39:44 -0300

    Html page nearly complete for Mass prediction, still working on passing the params to the model

--- server/meteoro/auth.py
+++ server/meteoro/auth.py
@@ -1,4 +1,5 @@
 import functools
+import pandas as pd
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
@@ -8,5 +9,27 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/', methods=('GET', 'POST'))
 def register():
+    recclass = request.args.get('recclass')
+    year = request.args.get('year')
+    reclat = request.args.get('reclat')
+    reclong = request.args.get('reclong')
+    
+    if all(value is not None for value in [recclass, year, reclat, reclong]):
+        infoArray = {
+            "recclass": [recclass],
+            "year": [year],
+            "reclat": [reclat],
+            "reclong": [reclong]
+        }
+    
+        df = pd.DataFrame(infoArray)
+    
+        print(df)
+        
+    else:
+        print("Um ou mais valores não estão preenchidos")  
+    
     return render_template('index.html')
+
+
 
