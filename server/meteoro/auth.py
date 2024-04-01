6dc4b6d7a0bb1d20d6a8fe43cf2ff5f6de67ccb4
Eric Coutinho <eric.coutinhop@hotmail.com>
2024-04-01 16:15:56 -0300

    Model working and showing information on html page

--- server/meteoro/auth.py
+++ server/meteoro/auth.py
@@ -7,30 +7,33 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+bp = Blueprint('auth', __name__, url_prefix='/')
 
-@bp.route('/', methods=('GET', 'POST'))
-def register():
+@bp.route('/', methods = ['GET', 'POST'])
+def index(): 
+    # return render_template('index.html')
+
+# @bp.route('/', methods=[])
+# def register():
     
-    metric = request.args.get('metric')
-    
-    match metric:
-        case 'Mass':
-            MassPredict()
-        case 'Coordenadas':
-            CoordPredict()
-        case 'Data':
-            DatePredict()
-    
-    
-    return render_template('index.html')
+    answer = ""
+    if request.method == "POST":
+        metric = request.form.get('metric')
+        match metric:
+            case "massa":
+                answer = MassPredict()
+            case "latitude":
+                answer = LatPredict()
+            case "longitude":
+                answer = LongPredict()    
+    return render_template('index.html', answer=answer)
 
 
 def MassPredict():
-    recclass = request.args.get('recclass')
-    year = request.args.get('year')
-    reclat = request.args.get('reclat')
-    reclong = request.args.get('reclong')
+    recclass = request.form.get('recclass')
+    year = request.form.get('year')
+    reclat = request.form.get('reclat')
+    reclong = request.form.get('reclong')
     
     le = load(open("label_encoder.pkl", 'rb'))
     
@@ -52,36 +55,74 @@ def MassPredict():
         
         answer = str(predict[0])
         print(answer)
+        
+        return answer
         
     else:
         print("Um ou mais valores não estão preenchidos")
         
-def CoordPredict():
-    recclass = request.args.get('recclass')
-    year = request.args.get('year')
-    reclat = request.args.get('reclat')
-    reclong = request.args.get('reclong')
+def LatPredict():
+    recclass = request.form.get('recclass')
+    mass = request.form.get('massa')
+    year = request.form.get('year')
+    long = request.form.get('long')
     
     le = load(open("label_encoder.pkl", 'rb'))
     
-    if all(value is not None for value in [recclass, year, reclat, reclong]):
+    if all(value is not None for value in [recclass, year]):
         infoArray = {
             "recclass": [recclass],
+            "mass": [mass],
             "year": [year],
-            "reclat": [reclat],
-            "reclong": [reclong]
+            "reclong": [long]
         }
     
         df = pd.DataFrame(infoArray)
         
         df['recclass'] = le.transform(df['recclass'])
         
-        loaded_model = load(open("randomForestMass.pkl", 'rb'))
+        loaded_model = load(open("decisionTreeLat.pkl", 'rb'))
         
         predict = loaded_model.predict(df)
         
         answer = str(predict[0])
-        print(answer)
+        
+        print("lat: ", answer)
+        
+        return answer
+        
+    else:
+        print("Um ou mais valores não estão preenchidos")
+
+
+def LongPredict():
+    recclass = request.form.get('recclass')
+    mass = request.form.get('massa')
+    year = request.form.get('year')
+    lat = request.form.get('lat')
+    
+    le = load(open("label_encoder.pkl", 'rb'))
+    
+    if all(value is not None for value in [recclass, year]):
+        infoArray = {
+            "recclass": [recclass],
+            "mass": [mass],
+            "year": [year],
+            "reclat": [lat]
+        }
+    
+        df = pd.DataFrame(infoArray)
+        
+        df['recclass'] = le.transform(df['recclass'])
+        
+        loaded_model = load(open("decisionTreeLong.pkl", 'rb'))
+        
+        predict = loaded_model.predict(df)
+        
+        answer = str(predict[0])
+        
+        print("long: ", answer)
+        return answer
         
     else:
         print("Um ou mais valores não estão preenchidos")
